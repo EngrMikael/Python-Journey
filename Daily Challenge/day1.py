f5717b3b6cb936dab471f0670e668a5d5de0b2b6
Michael John P. Tadeo, CpE <prof.engr.tadeomj@gmail.com>
2025-10-14 20:39:06 +0800

    Update day1.py for the documentation
    I added Documentation for each line and code block using Mintlify Doc Writer

--- Daily Challenge/day1.py
+++ Daily Challenge/day1.py
@@ -33,20 +33,46 @@ You can use .replace() or re.sub() to remove punctuation.
 A dictionary is your best friend here.
 '''
 
+#Documented using "Mintlify Doc Writer"
+
+# `import re` is importing the regular expression module in Python. This module allows you to work
+# with regular expressions, which are powerful tools for pattern matching and text manipulation. In
+# this specific case, `re` is used to remove punctuation from the input sentence using the `re.sub()`
+# function.
 import re
 
+# The line `sentence = "Hello world, hello ChatGPT! The world is big, and the world is round."` is
+# assigning a string to the variable `sentence`. The string contains a sentence with words separated
+# by spaces and punctuation marks like commas and exclamation marks. This sentence will be used as
+# input for the word frequency counter program.
 sentence = "Hello world, hello ChatGPT! The world is big, and the world is round."
 
+# The line `no_comma = re.sub(r"[,!]", "", sentence)` is using the `re.sub()` function from the `re`
+# module to remove commas and exclamation marks from the `sentence` string.
 no_comma = re.sub(r"[,!]", "", sentence)
 
+# `split_word = no_comma.split()` is splitting the modified sentence `no_comma` into a list of words.
+# The `split()` method is called on the `no_comma` string, which separates the string into a list of
+# words based on whitespace (spaces). This creates a list where each element is a word from the
+# sentence without any commas or exclamation marks.
 split_word = no_comma.split()
 
+# `word_counter = {}` is initializing an empty dictionary named `word_counter`. This dictionary will
+# be used to store the count of each word in the input sentence. Each word encountered in the sentence
+# will be a key in the dictionary, and the corresponding value will be the frequency of that word in
+# the sentence.
 word_counter = {}
 
+# This part of the code is iterating over each word in the `split_word` list, which contains all the
+# words from the input sentence without punctuation.
 for word in split_word:
     if word in word_counter:
         word_counter[word] += 1
     else:
         word_counter[word] = 1
 
+# `print(word_counter)` is displaying the contents of the `word_counter` dictionary, which contains
+# the count of each word in the input sentence. When this line is executed, it will output the
+# dictionary showing each unique word as a key and the number of times it appears in the sentence as
+# the corresponding value.
 print(word_counter)
