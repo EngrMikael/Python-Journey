2cf443593f489792a5962e3fc9cd3b063b76e248
Michael John P. Tadeo, CpE <prof.engr.tadeomj@gmail.com>
2025-10-15 19:26:58 +0800

    Update day2.3.py

--- Daily Challenge/day2.3.py
+++ Daily Challenge/day2.3.py
@@ -1,1 +1,20 @@
+'''
+Level 3: “Simple Login System” (if + dict)
+Problem:
 
+You have a dictionary of usernames and passwords:
+
+accounts = {"michael": "pass123", "john": "hello", "admin": "root"}
+
+
+Ask the user to input a username and password.
+Then:
+
+If username exists and password matches → print "Access Granted "
+
+If username exists but password is wrong → print "Wrong password "
+
+If username doesn’t exist → print "User not found "
+
+Goal: Combine conditionals with dictionary lookups.
+'''
