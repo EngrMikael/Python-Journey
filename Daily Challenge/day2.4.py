22aa399a418fa51a87c2b4d6cb276ea53f773c62
Michael John P. Tadeo, CpE <prof.engr.tadeomj@gmail.com>
2025-10-15 19:28:28 +0800

    Update day2.4.py

--- Daily Challenge/day2.4.py
+++ Daily Challenge/day2.4.py
@@ -1,1 +1,25 @@
+'''
+Level 4: “Student Grader” (loop + if + efficiency)
+Problem:
 
+Given a dictionary of student scores:
+
+grades = {"Anna": 87, "Ben": 92, "Chris": 74, "Diana": 59}
+
+
+You must print:
+
+"Anna: A" if score ≥ 90
+
+"Ben: B" if 80–89
+
+"Chris: C" if 70–79
+
+"Diana: F" if below 70
+
+Hint:
+
+Use a loop through the dictionary and nested if statements.
+
+Goal: Practice looping through dictionaries and applying multi-branch logic.
+'''
