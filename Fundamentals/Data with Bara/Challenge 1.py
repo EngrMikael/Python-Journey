c58122d6ddb0170295fdab33bdc866262ac9b62e
Michael John P. Tadeo, CpE <prof.engr.tadeomj@gmail.com>
2025-10-20 15:08:25 +0800

    Reorganize Repository and Adding the first Challenge from Baraa

---
+++ Fundamentals/Data with Bara/Challenge 1.py
@@ -0,0 +1,10 @@
+# - Use print() to create this exact output
+# - You are allowed to use only one print()
+# Output:
+#     Your Learning Path:
+#         - Python Basics
+#         - Data Engineering
+#         - AI
+
+
+print("Your Learning Path:\n\t- Python Basics\n\t- Data Engineering \n\t- AI")
