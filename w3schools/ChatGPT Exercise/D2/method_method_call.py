08b5ecf1d92a7909a750b11be944f80e6658abab
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-10-25 18:40:59 +0800

    unfinished practice challenge, to be completed tomorrow

---
+++ w3schools/ChatGPT Exercise/D2/method_method_call.py
@@ -0,0 +1,10 @@
+# Exercise 5 — Method Calling Another Method
+
+# Create a class Calculator with:
+
+# A method add(a, b) that returns the sum
+
+# A method display_sum(a, b) 
+# that calls add() internally and prints the result
+
+
