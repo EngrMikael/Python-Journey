2c6de8eaf89063ff41a32d16da65825d9b0de49e
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-10-26 20:03:29 +0800

    Empty Class that doesn't cause an error

---
+++ w3schools/ChatGPT Exercise/D3/empty_class_with_pass.py
@@ -0,0 +1,9 @@
+# Exercise 8 — Pass Statement
+
+# Create an empty class named Placeholder 
+# that doesn’t cause an error when run.
+
+class Placeholder:
+    def __init__(self) -> None:
+        pass
+    
