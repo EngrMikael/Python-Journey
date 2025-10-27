463948e744b5e9616dc699c95c866d4ded6c04be
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-10-27 18:53:33 +0800

    unfinished exercise

---
+++ w3schools/ChatGPT Exercise/D4/class_modify_property.py
@@ -0,0 +1,10 @@
+# Exercise 3: Modify Class Property
+
+# Now:
+
+# Change Car.wheels to 6
+
+# Print both cars’ wheels again.
+
+# Hint: This should affect all cars 
+# (unless they have their own instance version).
