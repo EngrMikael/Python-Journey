9cd1033d1fe53ae1da2356c126bb1d5293769b90
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-10-29 18:16:04 +0800

    Car class that has a pre assigned value for year

--- w3schools/ChatGPT Exercise/D4/instance_modify.py
+++ w3schools/ChatGPT Exercise/D4/instance_modify.py
@@ -8,4 +8,18 @@
 
 # Hint: Instance properties 
 # belong to each object individually.
-# i was busy today i will not be able to update
+
+class Car:
+    def __init__(self, model, year = 2025):
+        self.model = model
+        self.year = year
+    def display_info(self):
+        print(f"\nModel: {self.model}\nYear: {self.year}")
+
+car1_model = input("Enter Model: ")
+car1_year = int(input("Enter Year: "))
+car1_info = Car(car1_model, car1_year)
+car2_model = input("Enter Model: ")
+car2_info = Car(car1_model)
+car1_info.display_info()
+car2_info.display_info()
