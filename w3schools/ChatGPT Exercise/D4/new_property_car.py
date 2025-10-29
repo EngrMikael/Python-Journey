d5f3de38707a9b8a5bef507b425964040d50a3be
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-10-29 18:35:12 +0800

    adding a color property for class in the main code

--- w3schools/ChatGPT Exercise/D4/new_property_car.py
+++ w3schools/ChatGPT Exercise/D4/new_property_car.py
@@ -8,5 +8,15 @@
 # Then print it.
 # Now print car2.color — what happens?
 
-# ✅ Goal: Understand dynamic property addition.
-# i was busy today i will not be able to update
+# Goal: Understand dynamic property addition.
+class Car:
+    def __init__(self, model, year = 2025):
+        self.model = model
+        self.year = year
+car1_model = input("Enter Car 1 Model: ")
+car2_model = input("Enter Car 2 Model: ")
+car1_info = Car(car1_model)
+car2_info = Car(car2_model)
+car1_info.color = "Red"
+print(f"\nCar 1 Color: {car1_info.color}")
+print(f"Car 2 Color: {car2_info.color}")
